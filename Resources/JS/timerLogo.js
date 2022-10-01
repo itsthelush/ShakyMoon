05b92d3b80575edcf6c8dfcfc7417e338e12cf55
itsthelush <80926484+itsthelush@users.noreply.github.com>
2022-10-01 18:47:41 -0300

    Update timerLogo.js

--- Resources/JS/timerLogo.js
+++ Resources/JS/timerLogo.js
@@ -1,14 +1,5 @@
 const element = document.querySelector('.TeamLogo');
 
-/*function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
 
-async function timer() {
-  while (1==1) {
-      await setInterval(function(){element.style.opacity='1.0';}, 500);
-      await setInterval(function(){element.style.opacity='0.7';}, 500);
-  }
-}
-
-timer();8?
+setInterval(function(){element.style.opacity='1.0';}, 4000);
+setInterval(function(){element.style.opacity='0.7';}, 6000);

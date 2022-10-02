4599938ae6b2576cbdcea69aaf8e1bb8c0a3eea1
itsthelush <80926484+itsthelush@users.noreply.github.com>
2022-10-02 11:39:25 -0300

    Update timerLogo.js

--- Resources/JS/timerLogo.js
+++ Resources/JS/timerLogo.js
@@ -4,3 +4,14 @@ const element = document.querySelector('.TeamLogo');
     element.style.opacity='1.0';
     setInterval(function(){element.style.opacity='0.7';}, 2000);
   }*/
+  function sleep(milliseconds) {
+    const date = Date.now();
+    let currentDate = null;
+    do {
+      currentDate = Date.now();
+    } while (currentDate - date < milliseconds);
+  }
+  
+  console.log("Hello");
+  sleep(2000);
+  console.log("World!");
